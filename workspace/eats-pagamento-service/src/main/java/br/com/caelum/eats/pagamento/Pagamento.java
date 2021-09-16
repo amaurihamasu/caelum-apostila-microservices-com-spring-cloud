b1749b37eccf4cbb0a3a5ff6d5ee5dac930ad1b8
unknown <am.hamasu@gmail.com>
2021-09-15 22:26:50 -0300

    Client side load balancing no Feign do serviço de pagamentos com Ribbon

--- workspace/eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/Pagamento.java
+++ workspace/eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/Pagamento.java
@@ -10,10 +10,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
-
-import com.sun.istack.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
