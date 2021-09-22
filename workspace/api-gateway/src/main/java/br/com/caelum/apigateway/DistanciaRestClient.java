d2965066ec3707ae60d18e7148d1041790e2d01d
unknown <am.hamasu@gmail.com>
2021-09-21 22:36:17 -0300

    Circuit Breaker com Hystrix
    Exercício: Circuit Breaker com Hystrix

--- workspace/api-gateway/src/main/java/br/com/caelum/apigateway/DistanciaRestClient.java
+++ workspace/api-gateway/src/main/java/br/com/caelum/apigateway/DistanciaRestClient.java
@@ -6,17 +6,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @Service
 class DistanciaRestClient {
 
 	private RestTemplate restTemplate;
 	private String distanciaServiceUrl;
 
-	DistanciaRestClient(RestTemplate restTemplate, @Value("${configuracao.distancia.service.url}") String distanciaServiceUrl) {
+	DistanciaRestClient(RestTemplate restTemplate,
+			@Value("${configuracao.distancia.service.url}") String distanciaServiceUrl) {
 		this.restTemplate = restTemplate;
 		this.distanciaServiceUrl = distanciaServiceUrl;
 	}
 
+	@HystrixCommand
 	Map<String, Object> porCepEId(String cep, Long restauranteId) {
 		String url = distanciaServiceUrl + "/restaurantes/" + cep + "/restaurante/" + restauranteId;
 		return restTemplate.getForObject(url, Map.class);
